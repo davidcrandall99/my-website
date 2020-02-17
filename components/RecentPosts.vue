612ebf3aa910f693826f68bb69a522e26ebe5b7a
David Crandall <david@davidcrandall.com>
2020-02-17 09:32:45 -0800

    fixed loop with change to manifest

--- components/RecentPosts.vue
+++ components/RecentPosts.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
     <h1>Recent Posts</h1>
-    <div :key="i" v-for="i in manifest">
-    <nuxt-link :to="`/blog/${i.url}`">Click this</nuxt-link>
+    <div :key="index" v-for="(item, index) in manifest">
+    <nuxt-link :to="item.uri">{{ item.meta.data.title }}</nuxt-link>
     </div>
   </div>
 </template>
 
 <script>
 import { manifest } from "@/assets/manifest.js";
-console.log(manifest)
-let postURI = [];
-for (var i = 0; i in manifest; i++) {
-  let num = i.toString;
-  postURI.push(`/blog/${manifest[i].url}`);
-}
+
 
 export default {
     data() {
         return {
-            uris: postURI,
             manifest: manifest
         }
     }
