42e6fb5c9d84d90e8b9000364b159bd50cd205ea
David <david@davidcrandall.com>
2020-12-13 13:18:48 -0800

    blog page is complete
    there are global elements to add, but the page itself is done.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,20 +1,30 @@
 <template>
-	<div>
-		<AllPosts />
-	</div>
+  <div>
+    <section class="hero has-background-dark has-text-white">
+		<div class="container is-fullhd">
+			<div class="columns is-vcentered">
+			<div class="column meta is-full">
+				<h1 class="title is-1 has-text-white has-text-centered">Blog</h1>
+			</div>
+			</div>
+		</div>
+    </section>
+    <AllPosts />
+  </div>
 </template>
 
 <script>
 import AllPosts from "~/components/AllPosts";
 
-	export default {
-		components: {
-			AllPosts
-		}
-		
-	}
+export default {
+  components: {
+    AllPosts
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.hero {
+  height: 340px;
+}
 </style>
