91dbc93982c4936a0409274f64a2470befbac431
David <david@davidcrandall.com>
2021-01-14 11:15:35 -0800

    initial page
    no data

---
+++ components/Album.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+        <p>{{releaseDate}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: String, //name of the album
+            tracks: Array, //an Array of key/value pairs that denote track name, length, and a URL to the audio file to stream
+            releaseDate: Date, //the date the album was released
+            artwork: String //url of album artwork
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/music/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <album title="Composed" />
+    </div>
+</template>
+
+<script>
+import Album from '~/components/Album.vue';
+    export default {
+        components: {
+            Album
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
