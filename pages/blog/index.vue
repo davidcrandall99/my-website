6b40d082a5f601871d205b20e051b3e4ea274093
David <david@davidcrandall.com>
2021-04-13 18:47:59 -0700

    new background image for blog header

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <section class="hero has-background-dark has-text-white is-halfheight">
+    <section class="hero has-background-dark has-text-white">
+      <div class="overlay"></div>
 		<div class="container is-flex">
 			<div class="columns is-mobile is-vcentered is-centered is-full">
 				<div class="column has-text-centered is-full">
@@ -12,7 +13,24 @@
     <AllPosts />
   </div>
 </template>
-
+<style lang="scss" scoped>
+.hero {
+  background-image: url('/images/bloghero.jpeg');
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  height: 75vh;
+  .overlay {
+    width: 100vw;
+    height: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: rgba(0,0,0,0.5);
+  }
+}
+</style>
 <script>
 import AllPosts from "~/components/AllPosts";
 
