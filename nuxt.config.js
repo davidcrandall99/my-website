da115562f6b6d4eb6fccc5ed3c6fcc97ce49b1ce
David <david@davidcrandall.com>
2021-04-26 20:51:30 -0700

    changing sitemap url
    forcing google to take another hard look at my sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -118,6 +118,7 @@ export default {
   sitemap: {
     hostname: "https://davidcrandall.com",
     gzip: true,
+    path: 'sitemap-main.xml',
 
     filter ({ routes }) {
       return routes.map(route => {
