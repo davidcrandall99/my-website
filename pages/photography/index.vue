9caadd341240265f94e27444d8c410583f1bbe9e
David <david@davidcrandall.com>
2022-06-01 12:11:56 -0700

    adding new component to pages + update homepage copy

--- pages/photography/index.vue
+++ pages/photography/index.vue
@@ -1,108 +1,90 @@
 <template>
-<div>
-    <simple-hero bgImage="https://live.staticflickr.com/65535/48650271863_2f267b8055_k_d.jpg" title="Photography" />
+  <div>
+    <simple-hero
+      bgImage="https://live.staticflickr.com/65535/48650271863_2f267b8055_k_d.jpg"
+      title="Photography"
+    />
     <slanted-section>
-        <div class="container">
-            <div class="grid">
-                <div class="item" :key="index" v-for="set, index in photosets" :style="`background-image:url(${set.thumbnail})`">
-                    <nuxt-link class="link" :to="`/photography/gallery/${set.id}/`"></nuxt-link>
-                    <nuxt-link class="text" :to="`/photography/gallery/${set.id}/`">{{ set.title }}</nuxt-link>
-                </div>
-            </div>
-        </div>
+      <photo-album-list />
     </slanted-section>
-</div>
+  </div>
 </template>
 
 <script>
-import SimpleHero from '~/components/SimpleHero.vue';
+import SimpleHero from "~/components/SimpleHero.vue";
 export default {
-    head() {
-        return {
-            title: 'Photography by David Crandall',
-            script:[
-                { 
-                type: 'application/ld+json', 
-                json: {
-                "@context"  : "http://schema.org",
-                "@type"     : "Person",
-                "name"      : "David Crandall",
-                "mainEntityOfPage": `https://davidcrandall.com`
-                } 
-            },{ 
-                type: 'application/ld+json', 
-                json: {
-                "@context"  : "http://schema.org",
-                "@type"     : "Brand",
-                "name"      : "David Crandall Photography",
-                "mainEntityOfPage": `https://davidcrandall.com`
-                } 
-            }
-            ]
-        }
-    },
+  head() {
+    return {
+      title: "Photography by David Crandall",
+      script: [
+        {
+          type: "application/ld+json",
+          json: {
+            "@context": "http://schema.org",
+            "@type": "Person",
+            name: "David Crandall",
+            mainEntityOfPage: `https://davidcrandall.com`,
+          },
+        },
+        {
+          type: "application/ld+json",
+          json: {
+            "@context": "http://schema.org",
+            "@type": "Brand",
+            name: "David Crandall Photography",
+            mainEntityOfPage: `https://davidcrandall.com`,
+          },
+        },
+      ],
+    };
+  },
   components: { SimpleHero },
-        data() {
-            return {
-                photosets: []
-            }
-        },
-        mounted() {
-            this.getPhotoSets();
-        },
-        methods: {
-            getPhotoSets() {
-                fetch('https://davidcrandall.com/api/getsets')
-                    .then(response => response.json())
-                    .then(data => { this.photosets = data })
-            }
-        }
-    }
+};
 </script>
 
 <style lang="scss" scoped>
-    .grid {
-        display: flex;
-        width: 100%;
-        flex-wrap: wrap;
-        .item {
-            width: 25%;
-            height: 300px;
-            display: flex;
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: top center;
-            position: relative;
-            box-sizing: border-box;
-            padding: 20px;
-            overflow: hidden;
-            @media (max-width: 800px) {
-                width: 50%;
-            }
-            @media (max-width: 600px) {
-                width: 100%;
-            }
-            .link {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                background-color: rgba(0,0,0,0.2);
-                transition: all 0.5s ease;
-            }
-            .text {
-                position:absolute;
-                color: white;
-                bottom: 20px;
-                left: 20px;
-                font-size: 2rem;
-            }
-            &:hover {
-                .link {
-                    background-color: rgba(0,0,0,0.8)
-                }
-            }
-        }
+.grid {
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  .item {
+    width: 25%;
+    height: 300px;
+    display: flex;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top center;
+    position: relative;
+    box-sizing: border-box;
+    padding: 20px;
+    overflow: hidden;
+    @media (max-width: 800px) {
+      width: 50%;
     }
+    @media (max-width: 600px) {
+      width: 100%;
+    }
+    .link {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.2);
+      transition: all 0.5s ease;
+    }
+    .text {
+      position: absolute;
+      color: white;
+      bottom: 20px;
+      left: 20px;
+      font-size: 2rem;
+    }
+    &:hover {
+      .link {
+        background-color: rgba(0, 0, 0, 0.8);
+      }
+    }
+  }
+}
 </style>
